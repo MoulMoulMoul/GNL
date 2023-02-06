109d7e60a7bd604f65fe5625626f41fd66408ab0
MoulMoulMoul <MoulMoulMoul@hotmail.com>
2023-02-06 16:52:07 +0100

    gdeihgidf

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmoulin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 09:53:28 by nmoulin           #+#    #+#             */
+/*   Updated: 2023/01/07 03:33:14 by nmoulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	if (str == NULL)
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_strchr(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i++])
+		if (str[i] == c)
+			return (1);
+	return (0);
+}
+
+int	count_line(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*res;
+	size_t	lens1;
+
+	lens1 = ft_strlen(s1);
+	res = malloc(sizeof(char) * (lens1 + ft_strlen(s2)) + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	j = 0;
+	if (s1 != NULL)
+	{
+		while (s1[j++])
+			res[j] = s1[j];
+	}
+	j = 0;
+	i = 0;
+	while (s2[i])
+		res[j++] = s2[i++];
+	res[j] = '\0';
+	free(s1);
+	return (res);
+}
